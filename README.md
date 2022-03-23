490eecff403eb97b7f88268628390a6f3c277cde
michelle-aguirre <94195510+michelle-aguirre@users.noreply.github.com>
2022-03-23 13:48:38 -0400

    debugged work hours and timeblock issue

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # work-day-scheduler
+
+## Deploy
+
+[Deployed Link](https://michelle-aguirre.github.io/michelle_portfolio/)
+
+## Description
+
+In this project I created a workday scheduler using JQuery and Javascript. This scheduler allows you to input information per time block and save it. 
+
+## Image
+
+	![wd scheduler](../images/wd-scheduler.png)
+
+## Table of Contents
+
+## Technology
+
+HTML and CSS
+script.js
+Jquery
+Bootstrap
+
+## Installation
+moment.js
+
+## Usage
+
+## Credits and Contributors
+
+## Testing
+
+## Questions and Contacts
+
+If you have any questions I can be contacted via email. 
+Michelle Aguirre
+
+[michelleaguirre875@gmail.com](mailto:michelleaguirre875@gmail.com)
--- script.js
+++ script.js
@@ -1,8 +1,56 @@
+//jquery starter code
+$(document).ready(function(){ 
 
-$( "#currentDay" ).text( moment().format('MMMM Do YYYY, h:mm:ss a') );
-$( ".saveBtn" ).on( "click", function(  ) {
-var hour = $(this).parent().attr('id');
-var text = $(this).sibling('.description');
-localStorage.setItem(hour, text);
-  });
-$('#hour-8 .description').val(localStorage.getItem ('hour-8'));
+    $(".saveBtn" ).on( "click", function() { 
+
+        var text = $(this).siblings('.description').val();    
+        var hour = $(this).parent().attr('id');
+
+//local storage set up         
+        localStorage.setItem(hour, text);
+
+    });
+
+    function checkTime(){
+    var timeNow = moment().hours();
+
+    $(".time-block" ).each(function() {
+        var blockTime = parseInt($(this).attr("id").split("-")[1]);
+
+        //To check the time and add the classes for background
+        if (blockTime < timeNow) {
+            $(this).addClass("past");
+        }
+        else if(blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+checkTime();
+
+var interval = setInterval(checkTime, 15000);
+
+
+    //Get item from local storage if any
+    $("#hour-8 .description").val(localStorage.getItem)("hour-8");
+    $("#hour-9 .description").val(localStorage.getItem)("hour-9");
+    $("#hour-10 .description").val(localStorage.getItem)("hour-10");
+    $("#hour-11 .description").val(localStorage.getItem)("hour-11");
+    $("#hour-12 .description").val(localStorage.getItem)("hour-12");
+    $("#hour-13 .description").val(localStorage.getItem)("hour-13");
+    $("#hour-14 .description").val(localStorage.getItem)("hour-14");
+    $("#hour-15 .description").val(localStorage.getItem)("hour-15");
+    $("#hour-16 .description").val(localStorage.getItem)("hour-16");
+
+
+    $("#currentDay" ).text( moment().format('MMMM Do YYYY, h:mm:ss a') );
+
+
+});
+
